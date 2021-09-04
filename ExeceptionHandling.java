4a1f66ccc950bd9cc6dbe127707ecc4c11c62174
Sudeep Kumar Srivastava <79020871+sudeep-23-11@users.noreply.github.com>
2021-09-04 12:23:11 +0530

    Add files via upload

---
+++ ExeceptionHandling.java
@@ -0,0 +1,62 @@
+//ExeceptionHandling
+import java.util.Scanner;
+public class ExeceptionHandling
+{
+    public static void main(String[] args)
+    {
+        int i, n, x;
+        String s;
+        Scanner Sc=new Scanner(System.in);
+        for(i=0;i<5;i++)
+        {
+            n=Sc.nextInt();
+            try
+            {
+                try
+                {
+                    if(n%3==2)
+                    {
+                        s=null;
+                        System.out.println(s.length());
+                    }
+                    else if(n%3==1)
+                    {
+                        int arr[]=new int[5];
+                        arr[5]=0;
+                        System.out.println(arr[5]);
+                    }
+                    else
+                    {
+                        x=5/0;
+                        System.out.println(x);
+                    }
+                }
+                catch(ArrayIndexOutOfBoundsException e)
+                {
+                    System.out.println(e);
+                    System.out.println(e.getMessage());
+                }
+                catch(ArithmeticException e)
+                {
+                    System.out.println(e);
+                    System.out.println(e.getMessage());
+                }
+                finally
+                {
+                    System.out.println("Array Index out of Bounds Exception Handled");
+                    System.out.println("Arithmetic Exception Handled");
+                }
+            }
+            catch(NullPointerException e)
+            {
+                System.out.println(e);
+                System.out.println(e.getMessage());
+            }
+            finally
+            {
+                System.out.println("Null Pointer Exception Handled");
+            }
+        }
+        Sc.close();
+    }
+}

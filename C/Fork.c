dbc3cf25dd90ce4ab362b08d1228e17049da47fd
Sudeep Kumar Srivastava <79020871+sudeep-23-11@users.noreply.github.com>
2022-03-05 11:11:57 +0530

    Add files via upload

--- C/Fork.c
+++ C/Fork.c
@@ -9,13 +9,17 @@ int main()
     {
         printf("Inside the Child process\n");
         printf("fork function value = %d\n", f);
-        printf("pid = %d\n\n", getpid());
+        printf("uid = %d\n", getuid());
+        printf("pid = %d\n", getpid());
+        printf("ppid = %d\n\n", getppid());
     }
     else
     {
         printf("Inside the Parent process\n");
         printf("fork function value = %d\n", f);
-        printf("pid = %d\n\n", getpid());
+        printf("uid = %d\n", getuid());
+        printf("pid = %d\n", getpid());
+        printf("ppid = %d\n\n", getppid());
     }
     return 0;
 }

df1ddd03698211f5e7fd5c763556f6455b810c0e
Sudeep Kumar Srivastava <sudeepsri23@gmail.com>
2022-05-31 19:25:08 +0530

    Add files via upload

---
+++ C++/DSA/DP/LongestIncreasingSubsequences.cpp
@@ -0,0 +1,37 @@
+//LongestIncreasingSubsequences
+#include<iostream>
+#include<cstring>
+#include<vector>
+#include<set>
+using namespace std;
+int dp[1001][1001];
+int lcs(vector<int>X, vector<int>Y, int x, int y);
+int main()
+{
+    int i, N, x;
+    vector<int>a, b;
+    set<int>st;
+    cin>>N;
+    for(i=0;i<N;i++)
+    {
+        cin>>x;
+        a.push_back(x);
+        st.insert(x);
+    }
+    for(auto j:st)
+    b.push_back(j);
+    memset(dp, -1, sizeof(dp));
+    cout<<lcs(a, b, a.size(), b.size())<<endl;
+    return 0;
+}
+int lcs(vector<int>X, vector<int>Y, int x, int y)
+{
+    if(x==0||y==0)
+    return 0;
+    else if(dp[x-1][y-1]!=-1)
+    return dp[x-1][y-1];
+    else if(X.at(x-1)==Y.at(y-1))
+    return dp[x-1][y-1]=lcs(X, Y, x-1, y-1)+1;
+    else
+    return dp[x-1][y-1]=max(lcs(X, Y, x-1, y), lcs(X, Y, x, y-1));
+}
---
+++ C++/DSA/Graph/DisjointSets.cpp
@@ -0,0 +1,57 @@
+//DisjointSets
+#include<iostream>
+using namespace std;
+int parent[100001];
+int _rank[100001];
+void makeset(int n);
+int findparent(int n);
+void _union(int u, int v);
+int main()
+{
+    int n, m, u, v;
+    cin>>n>>m;
+    makeset(n);
+    while(m--)
+    {
+        cout<<"Make union"<<endl;
+        cin>>u>>v;
+        _union(u, v);
+        cout<<"Check components"<<endl;
+        cin>>u>>v;
+        if(findparent(u)==findparent(v))
+        cout<<"Same component"<<endl;
+        else
+        cout<<"Different components"<<endl;
+    }
+    return 0;
+}
+void makeset(int n)
+{
+    int i;
+    for(i=1;i<=n;i++)
+    {
+        parent[i]=i;
+        _rank[i]=0;
+    }
+}
+int findparent(int n)
+{
+    if(parent[n]==n)
+    return n;
+    else
+    return parent[n]=findparent(parent[n]);
+}
+void _union(int u, int v)
+{
+    u=findparent(u);
+    v=findparent(v);
+    if(_rank[u]<_rank[v])
+    parent[u]=v;
+    else if(_rank[u]>_rank[v])
+    parent[v]=u;
+    else
+    {
+        parent[v]=u;
+        _rank[u]++;
+    }
+}

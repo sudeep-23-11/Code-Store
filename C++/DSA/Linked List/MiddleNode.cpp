101f5473758df75659cbaeec23025f4eaf4738d1
Sudeep Kumar Srivastava <79020871+sudeep-23-11@users.noreply.github.com>
2022-06-07 02:56:01 +0530

    Add files via upload

--- C++/DSA/Linked List/MiddleNode.cpp
+++ C++/DSA/Linked List/MiddleNode.cpp
@@ -1,19 +1,10 @@
 //MiddleNode
 #include"LL.h"
-void middle(struct node *head);
 int main()
 {
-    int A[]={10, 20, 30, 40, 50};
-    head=convert(A, 5);
-    middle(head);
-    int B[]={10, 20, 30, 40, 50, 60};
-    head=convert(B, 6);
-    middle(head);
-    return 0;
-}
-void middle(struct node *head)
-{
     struct node *f, *s;
+    int A[]={10, 20, 30, 40, 50, 60};
+    head=convert(A, 6);
     f=head;
     s=head;
     while((f!=NULL)&&(f->next!=NULL))
@@ -22,4 +13,5 @@ void middle(struct node *head)
         f=f->next->next;
     }
     cout<<s->data<<endl;
+    return 0;
 }

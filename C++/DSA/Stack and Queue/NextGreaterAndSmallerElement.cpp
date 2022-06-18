faf0b36268d397b7e9fdd7306bd245e2670fecaf
Sudeep Kumar Srivastava <sudeepsri23@gmail.com>
2022-06-18 13:59:31 +0530

    Add files via upload

--- C++/DSA/Stack and Queue/NextGreaterAndSmallerElement.cpp
+++ C++/DSA/Stack and Queue/NextGreaterAndSmallerElement.cpp
@@ -2,55 +2,54 @@
 #include<iostream>
 #include<stack>
 using namespace std;
-stack<int>g, s;
+int i;
+stack<int>st;
 void _greater(int A[], int N);
 void smaller(int A[], int N);
 int main()
 {
-    int i, N;
+    int N;
     cin>>N;
     int A[N];
     for(i=0;i<N;i++)
     cin>>A[i];
-    cout<<"Next Greater Array"<<endl;
+    cout<<"Next Greater Element"<<endl;
     _greater(A, N);
-    cout<<"Next Smaller Array"<<endl;
+    while(st.empty()==0)
+    st.pop();
+    cout<<endl<<"Next Smaller Element"<<endl;
     smaller(A, N);
     return 0;
 }
 void _greater(int A[], int N)
 {
-    int i;
     int nge[N];
     for(i=(N*2-1);i>=0;i--)
     {
-        while((g.empty()==0)&&(g.top()<=A[i%N]))
-        g.pop();
-        if(g.empty()==0)
-        nge[i%N]=g.top();
+        while((st.empty()==0)&&(st.top()<=A[i%N]))
+        st.pop();
+        if(st.empty()==0)
+        nge[i%N]=st.top();
         else
         nge[i%N]=-1;
-        g.push(A[i%N]);
+        st.push(A[i%N]);
     }
     for(i=0;i<N;i++)
     cout<<nge[i]<<" ";
-    cout<<endl;
 }
 void smaller(int A[], int N)
 {
-    int i;
     int nse[N];
     for(i=(N*2-1);i>=0;i--)
     {
-        while((s.empty()==0)&&(s.top()>=A[i%N]))
-        s.pop();
-        if(s.empty()==0)
-        nse[i%N]=s.top();
+        while((st.empty()==0)&&(st.top()>=A[i%N]))
+        st.pop();
+        if(st.empty()==0)
+        nse[i%N]=st.top();
         else
         nse[i%N]=-1;
-        s.push(A[i%N]);
+        st.push(A[i%N]);
     }
     for(i=0;i<N;i++)
     cout<<nse[i]<<" ";
-    cout<<endl;
 }
---
+++ C++/DSA/Stack and Queue/SlidingWindowMaxAndMin.cpp
@@ -0,0 +1,44 @@
+//SlidingWindowMaxAndMin
+#include<iostream>
+#include<deque>
+using namespace std;
+deque<int>q;
+void max(int A[], int K, int x);
+void min(int A[], int K, int x);
+int main()
+{
+    int i, N, K;
+    cin>>N;
+    int A[N];
+    for(i=0;i<N;i++)
+    cin>>A[i];
+    cin>>K;
+    cout<<"Sliding Window Maximum"<<endl;
+    for(i=0;i<N;i++)
+    max(A, K, i);
+    q.clear();
+    cout<<endl<<"Sliding Window Minimum"<<endl;
+    for(i=0;i<N;i++)
+    min(A, K, i);
+    return 0;
+}
+void max(int A[], int K, int x)
+{
+    if((q.empty()==0)&&(q.front()==(x-K)))
+    q.pop_front();
+    while((q.empty()==0)&&(A[x]>A[q.back()]))
+    q.pop_back();
+    q.push_back(x);
+    if(x>=(K-1))
+    cout<<A[q.front()]<<" ";
+}
+void min(int A[], int K, int x)
+{
+    if((q.empty()==0)&&(q.front()==(x-K)))
+    q.pop_front();
+    while((q.empty()==0)&&(A[x]<A[q.back()]))
+    q.pop_back();
+    q.push_back(x);
+    if(x>=(K-1))
+    cout<<A[q.front()]<<" ";
+}

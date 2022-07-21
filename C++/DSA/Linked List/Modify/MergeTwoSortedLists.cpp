459d1c2a707c2190670d3eeedb8de8353229f56b
Sudeep Kumar Srivastava <sudeepsri23@gmail.com>
2022-07-22 01:12:34 +0530

    Add files via upload

--- C++/DSA/Linked List/Modify/MergeTwoSortedLists.cpp
+++ C++/DSA/Linked List/Modify/MergeTwoSortedLists.cpp
@@ -2,28 +2,28 @@
 #include"../LL.h"
 int main()
 {
-    struct node *h1, *h2;
+    struct node *t1, *t2;
     int A[]={10, 30, 50, 70, 90};
-    h1=convert(A, 5);
+    t1=convert(A, 5);
     int B[]={20, 40, 60, 80, 100};
-    h2=convert(B, 5);
-    if(h1->data>h2->data)
-    swap(h1, h2);
-    head=h1;
-    while(h1!=NULL)
+    t2=convert(B, 5);
+    if(t1->data>t2->data)
+    swap(t1, t2);
+    head=t1;
+    while(t1!=NULL)
     {
-        if(h1->data<=h2->data)
+        if(t1->data<=t2->data)
         {
-            temp=h1;
-            h1=h1->next;
+            temp=t1;
+            t1=t1->next;
         }
         else
         {
-            temp->next=h2;
-            swap(h1, h2);
+            temp->next=t2;
+            swap(t1, t2);
         }
     }
-    temp->next=h2;
+    temp->next=t2;
     display(head);
     return 0;
 }
--- C++/DSA/Linked List/Modify/RemoveDuplicatesFromSortedList.cpp
+++ C++/DSA/Linked List/Modify/RemoveDuplicatesFromSortedList.cpp
@@ -2,18 +2,21 @@
 #include"../LL.h"
 int main()
 {
-    struct node *t1;
+    struct node *t1, *t2;
     int A[]={10, 10, 20, 30, 30};
     head=convert(A, 5);
-    temp=head;
-    while(temp!=NULL)
+    t1=head;
+    t2=head->next;
+    while(t2!=NULL)
     {
-        t1=temp->next;
-        while((t1!=NULL)&&(t1->data==temp->data))
-        t1=t1->next;
-        temp->next=t1;
-        temp=t1;
+        if(t1->data!=t2->data)
+        {
+            t1->next=t2;
+            t1=t2;
+        }
+        t2=t2->next;
     }
+    t1->next=t2;
     display(head);
     return 0;
 }

a47bf1e8f91b41f061d5923c075b5c2ab6ce77f0
Sudeep Kumar Srivastava <sudeepsri23@gmail.com>
2023-01-23 18:18:17 +0530

    Add files via upload

--- C++/DSA/Dynamic Programming/1D/HouseRobber.cpp
+++ C++/DSA/Dynamic Programming/1D/HouseRobber.cpp
@@ -7,13 +7,13 @@ int main()
 {
     int i, N, m1, m2;
     cin>>N;
-    int A[N], dp[N+1];
+    int A[N], dp[N];
     for(i=0;i<N;i++)
     cin>>A[i];
     memset(dp, -1, sizeof(dp));
-    m1=func(1, N-1, A, dp);
+    m1=func(0, N-2, A, dp);
     memset(dp, -1, sizeof(dp));
-    m2=func(2, N, A, dp);
+    m2=func(1, N-1, A, dp);
     cout<<max(m1, m2)<<endl;
     return 0;
 }
@@ -23,5 +23,5 @@ int func(int l, int r, int A[], int dp[])
     return 0;
     if(dp[r]!=-1)
     return dp[r];
-    return dp[r]=max((A[r-1]+func(l, r-2, A, dp)), func(l, r-1, A, dp));
+    return dp[r]=max((A[r]+func(l, r-2, A, dp)), func(l, r-1, A, dp));
 }
